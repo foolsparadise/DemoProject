543b4bc4f0ce7c21e43312dc50128b355eb9ef76
foolsparadise <23549415+foolsparadise@users.noreply.github.com>
2020-02-21 10:38:08 +0800

    Update and commit

--- DemoProject/DemoProject/AppDelegate.m
+++ DemoProject/DemoProject/AppDelegate.m
@@ -20,7 +20,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [self gotoMainView];
+    return YES;
 
+}
+ 
+-(void)gotoMainView
+{
 //////////////////////////////////////////////////////////////////////////////
     //if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 13.0) {
     if (@available(iOS 13, *)) {
@@ -38,7 +44,7 @@
     
 //////////////////////////////////////////////////////////////////////////////
     /* 
-    // 1, use 1 viewcontroller
+    // 方案1, use 1 viewcontroller
     UIViewController *rootVC = .....
     UINavigationController *rootNav  = [[UINavigationController alloc] initWithRootViewController: rootVC];
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
@@ -49,44 +55,52 @@
     */
 //////////////////////////////////////////////////////////////////////////////
     
-    // 2, use 1 tabcontroller
+    // 方案2, use 1 tabcontroller
 
     TabBarDemo1ViewController *vc1 = [[TabBarDemo1ViewController alloc] init];
     vc1.view.backgroundColor = [UIColor whiteColor];
-    vc1.tabBarItem.badgeValue = @"23";
-    vc1.title = @"Home";
+    //vc1.tabBarItem.badgeValue = @"23";
+    //vc1.title = @"Home";
     vc1.tabBarItem.image = ImageWithNameInBundleFile(@"tabbar_home",@"LCTabBarControllerMore");//[UIImage imageNamed:@"tabbar_home"];
     vc1.tabBarItem.selectedImage = ImageWithNameInBundleFile(@"tabbar_home_selected",@"LCTabBarControllerMore");//[UIImage imageNamed:@"tabbar_home_selected"];
 
-    UIViewController *vc2 = [[UIViewController alloc] init];
+    TabBarDemo2ViewController *vc2 = [[TabBarDemo2ViewController alloc] init];
     vc2.view.backgroundColor = [UIColor cyanColor];
-    vc2.tabBarItem.badgeValue = @"1";
-    vc2.title = @"Message";
+    //vc2.tabBarItem.badgeValue = @"1";
+    //vc2.title = @"Message";
     vc2.tabBarItem.image = ImageWithNameInBundleFile(@"tabbar_message_center",@"LCTabBarControllerMore");//[UIImage imageNamed:@"tabbar_message_center"];
     vc2.tabBarItem.selectedImage = ImageWithNameInBundleFile(@"tabbar_message_center_selected",@"LCTabBarControllerMore");//[UIImage imageNamed:@"tabbar_message_center_selected"];
 
     UIViewController *vc3 = [[UIViewController alloc] init];
     vc3.view.backgroundColor = [UIColor redColor];
-    vc3.title = @"Discover";
+    //vc3.title = @"Discover";
     vc3.tabBarItem.image = ImageWithNameInBundleFile(@"tabbar_discover",@"LCTabBarControllerMore");//[UIImage imageNamed:@"tabbar_discover"];
     vc3.tabBarItem.selectedImage = ImageWithNameInBundleFile(@"tabbar_discover_selected",@"LCTabBarControllerMore");//[UIImage imageNamed:@"tabbar_discover_selected"];
 
     UIViewController *vc4 = [[UIViewController alloc] init];
     vc4.view.backgroundColor = [UIColor yellowColor];
-    vc4.tabBarItem.badgeValue = @"99+";
-    vc4.title = @"Profile";
+    //vc4.tabBarItem.badgeValue = @"99+";
+    //vc4.title = @"Profile";
     vc4.tabBarItem.image = ImageWithNameInBundleFile(@"tabbar_profile",@"LCTabBarControllerMore");//[UIImage imageNamed:@"tabbar_profile"];
     vc4.tabBarItem.selectedImage = ImageWithNameInBundleFile(@"tabbar_profile_selected",@"LCTabBarControllerMore");//[UIImage imageNamed:@"tabbar_profile_selected"];
 
-    UINavigationController *navC1 = [[UINavigationController alloc] initWithRootViewController:vc1];
-    UINavigationController *navC2 = [[UINavigationController alloc] initWithRootViewController:vc2];
-    UINavigationController *navC3 = [[UINavigationController alloc] initWithRootViewController:vc3];
-    UINavigationController *navC4 = [[UINavigationController alloc] initWithRootViewController:vc4];
+//    UINavigationController *navC1 = [[UINavigationController alloc] initWithRootViewController:vc1];
+//    UINavigationController *navC2 = [[UINavigationController alloc] initWithRootViewController:vc2];
+//    UINavigationController *navC3 = [[UINavigationController alloc] initWithRootViewController:vc3];
+//    UINavigationController *navC4 = [[UINavigationController alloc] initWithRootViewController:vc4];
+    RTRootNavigationController *navC1 = [[RTRootNavigationController alloc] initWithRootViewController:vc1];
+    navC1.tabBarItem.title = NSStringLocalizedInfoPlist(@"View1");
+    RTRootNavigationController *navC2 = [[RTRootNavigationController alloc] initWithRootViewController:vc2];
+    navC2.tabBarItem.title = NSStringLocalizedInfoPlist(@"View2");
+    RTRootNavigationController *navC3 = [[RTRootNavigationController alloc] initWithRootViewController:vc3];
+    navC3.tabBarItem.title = NSStringLocalizedInfoPlist(@"View3");
+    RTRootNavigationController *navC4 = [[RTRootNavigationController alloc] initWithRootViewController:vc4];
+    navC4.tabBarItem.title = NSStringLocalizedInfoPlist(@"View4");
 
     LCTabBarController *tabBarC    = [[LCTabBarController alloc] init];
-    //    tabBarC.itemTitleFont          = [UIFont boldSystemFontOfSize:11.0f];
-    //    tabBarC.itemTitleColor         = [UIColor greenColor];
-    //    tabBarC.selectedItemTitleColor = [UIColor redColor];
+    tabBarC.itemTitleFont          = [UIFont fontWithName:@"NotoSansCJK-Regular" size:11.5]; //设置自定义字体
+    tabBarC.itemTitleColor         = [UIColor blackColor];
+    tabBarC.selectedItemTitleColor = [UIColor blackColor];
     //    tabBarC.itemImageRatio         = 0.5f;
     //    tabBarC.badgeTitleFont         = [UIFont boldSystemFontOfSize:12.0f];
     tabBarC.viewControllers        = @[navC1, navC2, navC3, navC4];
@@ -97,7 +111,7 @@
     
 //////////////////////////////////////////////////////////////////////////////
     /*
-    // 3, use rootnavcontroller
+    // 方案3, use rootnavcontroller
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     RTRootNavigationController *navigationController = [[RTRootNavigationController alloc] initWithRootViewController:[[AppRootViewController alloc] init]];
@@ -118,9 +132,45 @@
     
     NSArray * paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     itNSLog(@"language(%@)sandbox(%@)", NSStringLocalizedInfoPlist(@"LanguageTest"), paths[0]);
-    return YES;
+    [self printFontName]; //打印所有字体,其中有自添加的开源字体 思源黑体NotoSansCJK-Regular 等
+    
+    // 判断沙盒中是否存在广告图片，如果存在，直接显示
+    NSString *filePath = [AdImageTool getFilePathWithImageName:[kUserDefaults valueForKey:adImageName]];
+    BOOL isExist = [AdImageTool isFileExistWithFilePath:filePath];
+    if (isExist) {// 图片存在
+        AdvertiseView *advertiseView = [[AdvertiseView alloc] initWithFrame:self.window.bounds];
+        advertiseView.filePath = filePath;
+        [advertiseView show];
+    }
+    
+    // 无论沙盒中是否存在广告图片，都需要重新调用广告接口，判断广告是否更新,这一步在首页中完成。
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(pushToAd) name:@"pushtoad" object:nil];//广告页点击事件
+    [AdImageTool getAdvertisingImage];//判断广告是否更新 图片URL地址,在 AdImageTool.m getAdvertisingImage 中修改
+    
+
 }
 
+- (void)pushToAd {
+////    NSString *url = [SystemInfo getSystemInfoFoKey:@"LAUNCH_INTENT_URL"];//判断是否有需要跳转的广告页
+////    if (url.length > 0) {
+//        AdvertiseViewController *adVc = [[AdvertiseViewController alloc] init];
+////        adVc.adUrl = url;
+//        [self.navigationController pushViewController:adVc animated:YES];
+////    }
+    NSLog(@"pushtoAd 建议不要写在AppDelegate写在其它地方");
+    //UIWindow *window = [UIApplication sharedApplication].keyWindow;
+}
+// 遍历获取字体名称
+- (void)printFontName
+{
+    for(NSString *fontFamilyName in [UIFont familyNames]){
+        itNSLog(@"fontfamily:'%@'",fontFamilyName);
+        for(NSString *fontName in [UIFont fontNamesForFamilyName:fontFamilyName]){
+            itNSLog(@"\tfont:'%@'",fontName);
+        }
+        NSLog(@"-------------");
+    }
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- DemoProject/DemoProject/TabBarDemoViewController/TabBarDemo2ViewController.m
+++ DemoProject/DemoProject/TabBarDemoViewController/TabBarDemo2ViewController.m
@@ -8,7 +8,11 @@
 
 #import "TabBarDemo2ViewController.h"
 
-@interface TabBarDemo2ViewController ()
+@interface TabBarDemo2ViewController () <UISearchBarDelegate>
+
+@property (nonatomic, weak) UIButton *leftButton;
+@property (nonatomic, weak) UISearchBar *searchBar;
+@property (nonatomic, weak) UIButton *rightButton;
 
 @end
 
@@ -17,7 +21,102 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.title = @"TabBarDemo2ViewController";
+    [self initCurrentNavigationItem];
+    
 }
+- (void)initCurrentNavigationItem
+{
+    //搜索
+    UIView *titleView = [[UIView alloc] init];
+    UISearchBar *searchBar = [[UISearchBar alloc] initWithFrame:titleView.bounds];
+    [titleView addSubview:searchBar];
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 11.0) { // iOS 11
+        [NSLayoutConstraint activateConstraints:@[
+                                                  [searchBar.topAnchor constraintEqualToAnchor:titleView.topAnchor],
+                                                  [searchBar.leftAnchor constraintEqualToAnchor:titleView.leftAnchor],
+                                                  [searchBar.rightAnchor constraintEqualToAnchor:titleView.rightAnchor],
+                                                  [searchBar.bottomAnchor constraintEqualToAnchor:titleView.bottomAnchor]
+                                                  ]];
+    } else {
+        searchBar.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    }
+    self.navigationItem.titleView = titleView;
+    searchBar.placeholder = [NSBundle py_localizedStringForKey:@"PYSearchSearchPlaceholderText"];
+    searchBar.backgroundImage = [NSBundle py_imageNamed:@"clearImage"];
+    searchBar.delegate = self;
+    for (UIView *subView in [[searchBar.subviews lastObject] subviews]) {
+        if ([[subView class] isSubclassOfClass:[UITextField class]]) {
+            UITextField *textField = (UITextField *)subView;
+            textField.font = [UIFont systemFontOfSize:16];
+            //_searchTextField = textField;
+            break;
+        }
+    }
+    self.searchBar = searchBar;
+    self.navigationItem.titleView = self.searchBar;
+    //返回
+    UIButton *backButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    UIImage *backImage = [NSBundle py_imageNamed:@"back"];
+    backButton.titleLabel.font = [UIFont systemFontOfSize:17];
+    //[backButton setTitle:[NSBundle py_localizedStringForKey:@"Back"] forState:UIControlStateNormal];
+    [backButton setImage:backImage forState:UIControlStateNormal];
+    [backButton addTarget:self action:@selector(backDidClick)  forControlEvents:UIControlEventTouchUpInside];
+    backButton.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    backButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    [backButton.imageView sizeToFit];
+    backButton.contentEdgeInsets = UIEdgeInsetsMake(0, -ceil(backImage.size.width / 2.0), 0, 0);
+    backButton.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 0);
+    [backButton sizeToFit];
+    //backButton.py_width += 3;
+    self.leftButton = backButton;
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+    //取消
+    UIButton *cancleButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    cancleButton.titleLabel.font = [UIFont systemFontOfSize:17];
+    [cancleButton setTitle:[NSBundle py_localizedStringForKey:@"Cancel"] forState:UIControlStateNormal];
+    [cancleButton addTarget:self action:@selector(cancelDidClick)  forControlEvents:UIControlEventTouchUpInside];
+    cancleButton.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    cancleButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentRight;
+    [cancleButton sizeToFit];
+    //cancleButton.py_width += PYSEARCH_MARGIN;
+    self.rightButton = cancleButton;
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:cancleButton];
+    
+    self.navigationItem.titleView.backgroundColor = [UIColor yellowColor];
+}
+
+/*
+ - (UIViewController *)get1SearchVC
+ {
+        // 1. Create an Array of popular search
+         NSArray *hotSeaches = @[@"Java", @"Python", @"Objective-C", @"Swift", @"C", @"C++", @"PHP", @"C#", @"Perl", @"Go", @"JavaScript", @"R", @"Ruby", @"MATLAB"];
+         // 2. Create a search view controller
+         PYSearchViewController *searchViewController = [PYSearchViewController searchViewControllerWithHotSearches:hotSeaches searchBarPlaceholder:NSLocalizedString(@"PYExampleSearchPlaceholderText", @"搜索编程语言") didSearchBlock:^(PYSearchViewController *searchViewController, UISearchBar *searchBar, NSString *searchText) {
+             // Called when search begain.
+             // eg：Push to a temp view controller
+             NSLog(@"%@", searchText);
+         }];
+         // 3. Set style for popular search and search history
+         //searchViewController.hotSearchStyle = 0;
+         //searchViewController.searchHistoryStyle = PYHotSearchStyleDefault;
+         searchViewController.hotSearchStyle = PYHotSearchStyleDefault;
+         searchViewController.searchHistoryStyle = 0;
+         
+         // 4. Set delegate
+         searchViewController.delegate = self;
+         // 5. Present(Modal) or push search view controller
+         // Present(Modal)
+     //    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:searchViewController];
+     //    [self presentViewController:nav animated:YES completion:nil];
+         // Push
+         // Set mode of show search view controller, default is `PYSearchViewControllerShowModeModal`
+         searchViewController.searchViewControllerShowMode = PYSearchViewControllerShowModePush;
+     //    // Push search view controller
+     return searchViewController;
+ }
+ */
 
 /*
 #pragma mark - Navigation
